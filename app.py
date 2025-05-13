04a7cd3ab25610f733fed2e17f1df16fe70f32a4
Génesis Ojeda <genesismojeda@gmail.com>
2025-05-13 19:12:21 -0400

    Cambios finales en credenciales de Azure

--- app.py
+++ app.py
@@ -10,21 +10,7 @@ username = os.getenv("SQL_USERNAME")
 password = os.getenv("SQL_PASSWORD")
 driver = "{ODBC Driver 18 for SQL Server}"
 
-# server = "tcp:adminestudiantes1.database.windows.net,1433"
-# database = "databasepf1"
-# username = "adminpfestudiante"
-# password = "Alpha123@"
-# driver = "{ODBC Driver 18 for SQL Server}"
-
-print("Server:", os.getenv("SQL_SERVER"))
-print("Database:", os.getenv("SQL_DATABASE"))
-print("Username:", os.getenv("SQL_USERNAME"))
-print("Password:", os.getenv("SQL_PASSWORD"))
-
-
 connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
-# connection_string = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER=tcp:{os.getenv('SQL_SERVER')},1433;DATABASE={os.getenv('SQL_DATABASE')};UID={os.getenv('SQL_USERNAME')};PWD={os.getenv('SQL_PASSWORD')};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30"
-
 conn = pyodbc.connect(connection_string)
 cursor = conn.cursor()
 
